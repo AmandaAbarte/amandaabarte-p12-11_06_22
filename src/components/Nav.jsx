cde6347e6b02d8a1618f6fa09362d39f18258f0c
AmandaAbarte <amanda.abarte@gmail.com>
2022-06-16 12:38:35 +0200

    nav styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import logo from '../assets/logo.png'
 
 export default function Nav() {
     return (
         <header>
             <nav>
+
                 <ul>
+                    <img src={logo} alt="sportsee logo"></img>
                     <li>Home</li>
                     <li>Profile</li>
                     <li>Settings</li>

b74a77f5222d75a2bb4d4818df52789f55d73b07
AmandaAbarte <amanda.abarte@gmail.com>
2022-06-20 17:10:22 +0200

    bottom axis for line chart

--- src/components/Charts/AverageSessions.jsx
+++ src/components/Charts/AverageSessions.jsx
@@ -1,18 +1,19 @@
 import React, { useRef, useEffect, useState } from "react";
 import axios from "axios";
-import { select, line, curveCardinal} from "d3";
+import { select, line, curveCardinal, axisBottom, scaleLinear, index } from "d3";
 
 export default function AverageSessions(props) {
   const [userSessions, setUserSessions] = useState("");
+  const days = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
 
   const allSessions = userSessions && userSessions.sessions;
-//returns session lengths into an array
+  //returns session lengths into an array
   const sessionData =
     allSessions &&
     allSessions.map((item) => {
-      return item.sessionLength
+      return item.sessionLength;
     });
-    
+
   const getUserSessions = () => {
     axios
       .get(`http://localhost:3000/user/${props.userID}/average-sessions`)
@@ -31,26 +32,33 @@ export default function AverageSessions(props) {
 
   const svgRef = useRef();
   //called on loading and when data changes
-    useEffect(() => {
-      const svg = select(svgRef.current);
-
-      //generates "d" attribute of a path element
-      const myLine =line()
-      .x((value,index)=> index *50)
-      .y(value => 150 - value)
-      .curve(curveCardinal)
-        //renders path element and attaches "d" attribute from the line generator
-      svg
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    const xScale = scaleLinear()
+      .domain([0, sessionData.length-1])
+      .range([0, 250]);
+    const yScale = scaleLinear().domain([0, 70]).range([150, 0]);
+    const xAxis = axisBottom(xScale).ticks(sessionData.length).tickFormat(index => index +1);
+    svg.select(".xaxis").style("transform", "translateY(200px)").call(xAxis);
+    //generates "d" attribute of a path element
+    const myLine = line()
+      .x((value, index) => xScale(index))
+      .y((value) => yScale(value))
+      .curve(curveCardinal);
+    //renders path element and attaches "d" attribute from the line generator
+    svg
       .selectAll("path")
       .data([sessionData])
       .join("path")
-      .attr("d", value => myLine(value))
+      .attr("d", (value) => myLine(value))
       .attr("fill", "none")
-      .attr("stroke", "white")
-    }, [sessionData]);
+      .attr("stroke", "white");
+  }, [sessionData]);
   return (
     <div className="linechart-container">
-      <svg ref={svgRef} className="linechart"></svg>
+      <svg ref={svgRef} className="linechart">
+        <g className="xaxis"/>
+      </svg>
     </div>
   );
 }

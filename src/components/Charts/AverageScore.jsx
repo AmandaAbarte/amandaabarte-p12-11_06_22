3925bbbaba9eb5431e78e241114cb30c050b6661
AmandaAbarte <amanda.abarte@gmail.com>
2022-07-17 15:26:05 +0200

    average score chart

--- src/components/Charts/AverageScore.jsx
+++ src/components/Charts/AverageScore.jsx
@@ -1,12 +1,37 @@
 import React, { useRef, useEffect, useState } from "react";
-import axios from "axios";
-import { select } from "d3";
+import {
+  RadialBarChart,
+  RadialBar,
+  Legend,
+  ResponsiveContainer,
+  PolarAngleAxis
+} from "recharts";
 
 export default function AverageScore(props) {
+  // const [todayData, setTodayData] = useState(props.todayScore);
+  console.log(props.todayScore *100);
+  const data = [
+    {
+      value: props.todayScore ,
+      fill: "#FF0000",
+    },
+  ];
+  const style = {
+    top: "50%",
+    right: 0,
+    transform: "translate(0, -50%)",
+    lineHeight: "24px",
+  };
   return (
-    <div>
-        <svg></svg>
-        {/* todays score is {props.todayScore} */}
+    <div className="progress-chart-container">
+      <p> Score</p>
+      <ResponsiveContainer width="100%" height="100%" className='progress-chart'>
+            <RadialBarChart innerRadius="80%" barSize={10} data={data} startAngle={90} endAngle={450} >
+                <PolarAngleAxis type="number" domain={[0, 1]} tick={false} />
+                <RadialBar background dataKey="value" cornerRadius={5} fill={data.fill} />
+            </RadialBarChart>
+        </ResponsiveContainer>
+        <p className="progress-lable">{props.todayScore * 100} % <br/> of your Goal</p>
     </div>
-  )
+  );
 }
--- src/components/Charts/RadarChartOne.jsx
+++ src/components/Charts/RadarChartOne.jsx
@@ -10,14 +10,29 @@ import {
 } from "recharts";
 
 export default function RadarChartOne(props) {
-  const [userPerformance, setUserPerformance] = useState("");
+  const [userPerformance, setUserPerformance] = useState({
+    labels: [],
+    data: [],
+  });
   const getUserPerformance = () => {
     axios
       .get(`http://localhost:3000/user/${props.userID}/performance`)
 
       .then((response) => {
-        // console.log(response.data.data);
-        setUserPerformance(response.data.data);
+        const data = response.data.data.data.map((data) => {
+          return data.value;
+        });
+        // const data = Object.keys(response.data.data.kind).map(key => {
+        //   return key
+        // })
+        const label = Object.values(response.data.data.kind)
+        console.log(label);
+        setUserPerformance(
+          //
+          // [response.data.data.data, [response.data.data.kind]]
+          { labels: label, data: data }
+        );
+        console.log("data", userPerformance.data);
       })
       .catch((error) => {
         console.log(error);
@@ -27,33 +42,30 @@ export default function RadarChartOne(props) {
   useEffect(() => {
     getUserPerformance();
   }, []);
-  const performance = userPerformance && userPerformance.data
-  // const [data, setData] = useState()
-  // const allPerformanceData = userPerformance && userPerformance.data.map((item)=>{
-  //   return item.data.map((data)=>{
-  //     return data.value
-  //   })
-  // })
-  // const performanceData = allPerformanceData && allPerformanceData.map((data)=>{
-  //   return data.value
-  // })
-  // console.log(performanceData);
+  const performance = userPerformance && userPerformance.data;
   return (
     // <ResponsiveContainer width="250px" height="250px">
-    // <div className="radar-chart-container">
-      <RadarChart cx={50} cy={50} outerRadius={90} width={250} height={250} data={performance}>
+    <div className="radar-chart-container">
+      <RadarChart
+        cx={50}
+        cy={50}
+        outerRadius={90}
+        width={250}
+        height={250}
+        data={userPerformance}
+      >
         <PolarGrid />
-        <PolarAngleAxis dataKey="kind" />
+        <PolarAngleAxis dataKey="labels" />
         <PolarRadiusAxis />
         <Radar
           name="Mike"
-          dataKey="value"
+          dataKey="data"
           stroke="#8884d8"
           fill="#8884d8"
           fillOpacity={0.6}
         />
       </RadarChart>
-    // </div>
+    </div>
     // </ResponsiveContainer>
   );
 }

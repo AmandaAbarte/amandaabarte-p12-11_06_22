dd0354dd42b99ea37c41c8671123011179491a18
AmandaAbarte <amanda.abarte@gmail.com>
2022-07-18 20:54:52 +0200

    fix class names

--- src/components/Charts/RadarChartOne.jsx
+++ src/components/Charts/RadarChartOne.jsx
@@ -53,7 +53,7 @@ let data = sortArray(userPerformance.data, userPerformance.labels);
   return (
     // <ResponsiveContainer width="250px" height="250px">
     <div className="radar-chart-container">
-      <ResponsiveContainer width="100%" height="100%">
+      <ResponsiveContainer width="100%" height="100%" className="radar-chart">
         <RadarChart cx="50%" cy="50%" outerRadius="50%" data={data}>
           <PolarGrid />
           <PolarAngleAxis dataKey="type" stroke="white" tickLine={false} />

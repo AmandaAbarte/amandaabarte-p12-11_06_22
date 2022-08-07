49f9bc01e1a8174975b0d34f39239e2919ca2077
AmandaAbarte <amanda.abarte@gmail.com>
2022-08-07 13:38:50 +0200

    error styling

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -5,9 +5,9 @@ export default function Error() {
         <div className="error-container">
             <h1>No user found</h1>
             <p>Please navigate to</p>
-            <a href="/user/12">User 12</a>
-            <p>or</p>
-            <a href="/user/18">User 18</a>
+            
+            <p><a href="/user/12">User 12</a> or <a href="/user/18">User 18</a></p>
+            
         </div>
     )
 }

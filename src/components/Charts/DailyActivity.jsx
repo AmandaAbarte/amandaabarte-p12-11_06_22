9314f5ebd379faeed310f6cb3fd79681efe3f39b
AmandaAbarte <amanda.abarte@gmail.com>
2022-08-07 13:26:00 +0200

    daily activity lable placement

--- src/components/Charts/DailyActivity.jsx
+++ src/components/Charts/DailyActivity.jsx
@@ -1,42 +1,62 @@
 import React from "react";
 
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 export default function DailyActivity(props) {
- 
-  function sortArray(arrayData){
+  function sortArray(arrayData) {
     let result = [];
-    for(let i = 0; i<arrayData.length; i++){
-        result.push({calories: arrayData[i].calories, kilogram: arrayData[i].kilogram, day: i+1});
+    for (let i = 0; i < arrayData.length; i++) {
+      result.push({
+        calories: arrayData[i].calories,
+        kilogram: arrayData[i].kilogram,
+        day: i + 1,
+      });
     }
     return result;
-}
+  }
 
-let data = sortArray(props.data);
- 
-  
+  let data = sortArray(props.data);
+
   return (
     <div className="daily-chart-container">
-      <div className="barGraphHeader">
-            <h3 className="barGraphTitle">Daily Activity</h3>
-            <div className="barGraphLegend">
-                <div className="barGraphType">
-                    <div className="typeCircle typeCircle--black" />
-                    <p className="typeText">Weight (kg)</p>
-                </div>
-                <div className="barGraphType">
-                    <div className="typeCircle typeCircle--red" />
-                    <p className="typeText">Burned Calories (kCal)</p>
-                </div>
-            </div>
+      <div className="bar-graph-header">
+        <h3 className="bar-graph-title">Daily Activity</h3>
+        <div className="bar-graph-legend">
+          <div className="bar-graph-type">
+            <div className="circle circle-black"></div>
+            <p className="typeText">Weight (kg)</p>
+          </div>
+          <div className="bar-graph-type">
+            <div className="circle circle-red"></div>
+            <p className="typeText">Burned Calories (kCal)</p>
+          </div>
         </div>
-        <ResponsiveContainer width="100%" height="100%" className="daily-chart">
+      </div>
+      <ResponsiveContainer width="100%" height="100%" className="daily-chart">
         <BarChart data={data}>
-          <CartesianGrid strokeDasharray="3 2" stroke="#DEDEDE"/>
+          <CartesianGrid strokeDasharray="3 2" stroke="#DEDEDE" />
           <XAxis dataKey="day" stroke="#9B9EAC" />
           <YAxis stroke="#9B9EAC" />
-          <Bar dataKey="kilogram" fill="#282D30" radius={[25,25,0,0]} barSize={7}  />
-          <Bar dataKey="calories" fill="#E60000" radius={[25,25,0,0]} barSize={7} />
+          <Bar
+            dataKey="kilogram"
+            fill="#282D30"
+            radius={[25, 25, 0, 0]}
+            barSize={7}
+          />
+          <Bar
+            dataKey="calories"
+            fill="#E60000"
+            radius={[25, 25, 0, 0]}
+            barSize={7}
+          />
         </BarChart>
       </ResponsiveContainer>
     </div>

47ad5fb2faff03d7091561ab34dfe3ea2affd0d3
AmandaAbarte <amanda.abarte@gmail.com>
2022-08-07 16:33:26 +0200

    documentation

--- src/components/Charts/AverageSessions.jsx
+++ src/components/Charts/AverageSessions.jsx
@@ -1,27 +1,41 @@
 import React from "react";
 
-import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer } from "recharts";
 
 export default function AverageSessions(props) {
-
-  function sortArray(arrayData){
+  /**
+   * Get data for Average Session Chart
+   * @param {array} arrayData Session data
+   * @returns {array} Data corresponding to each day
+   */
+  function sortArray(arrayData) {
     let result = [];
-    const day = ["L","M","M","J","V","S","D"];
-    for(let i = 0; i<arrayData.length; i++){
-        result.push({day: day[i], sessionLength: arrayData[i].sessionLength});
+    const day = ["L", "M", "M", "J", "V", "S", "D"];
+    for (let i = 0; i < arrayData.length; i++) {
+      result.push({ day: day[i], sessionLength: arrayData[i].sessionLength });
     }
     return result;
-}
+  }
 
-let data = sortArray(props.data);
+  let data = sortArray(props.data);
 
-  
   return (
     <div className="linechart-container">
-       <ResponsiveContainer width="100%" height="100%">
-        <LineChart data={data} style={{backgroundColor: "red"}}>
-          <XAxis dataKey="day" stroke="white" axisLine={false} tickLine={false} />
-          <Line type="monotone" dot={false} dataKey="sessionLength" stroke="white" strokeWidth={1.5}/>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart data={data} style={{ backgroundColor: "red" }}>
+          <XAxis
+            dataKey="day"
+            stroke="white"
+            axisLine={false}
+            tickLine={false}
+          />
+          <Line
+            type="monotone"
+            dot={false}
+            dataKey="sessionLength"
+            stroke="white"
+            strokeWidth={1.5}
+          />
         </LineChart>
       </ResponsiveContainer>
     </div>
